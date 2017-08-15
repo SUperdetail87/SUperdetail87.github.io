94f9a03fdd4bdc805ab8444ffa28c92304cf8b2c
Dukerlin Cherestal <d13center@gmail.com>
2017-08-15 09:07:58 -0400

    javascript
    random file

---
+++ random.js
@@ -0,0 +1,15 @@
+/*
+
+  Random picture generator, Javascript Function 
+  Javascript Page
+  
+   Function List:
+   randomInteger()
+      Used to reverse the order of characters in a text string
+
+*/
+
+
+function randomInteger(size) {
+   return Math.floor((size+1)*Math.random());
+}
